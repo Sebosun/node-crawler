448b1e4180c6144e8faf24886607d7cc8f5b6ca4
Sebosun <sebos@protonmail.com>
2024-02-24 09:40:27 +0100

    fix: url crawling & tests

--- src/utils/getURLsFromHTML.test.ts
+++ src/utils/getURLsFromHTML.test.ts
@@ -10,6 +10,14 @@ test("returns abs links", () => {
 });
 
 test("returns abs links", () => {
+  const baseAnchorTag = `<a class="link" href="/posts/zen-of-proverbs/">The Zen of Proverbs</a>`;
+  const links = ["https://boot.dev/posts/zen-of-proverbs/"];
+  expect(getURLsFromHTML(baseAnchorTag, "https://boot.dev")).toStrictEqual(
+    links,
+  );
+});
+
+test("returns abs links", () => {
   const baseAnchorTag = `<a href="https://boot.dev/dev">Learn Backend Development</a>`;
   const links = ["https://boot.dev/dev"];
   expect(getURLsFromHTML(baseAnchorTag, "https://boot.dev")).toStrictEqual(
@@ -23,12 +31,14 @@ test("returns multiple links", () => {
   <a href="https://boot.dev/dev/kiev">Learn Backend Development</a>
   <a href="https://boot.dev/dev/lew">Learn Backend Development</a>
   <a href="https://boot.dev/dev/ref">Learn Backend Development</a>
+  <a class="link" href="/posts/zen-of-proverbs/">The Zen of Proverbs</a>
 `;
   const links = [
     "https://boot.dev/dev",
     "https://boot.dev/dev/kiev",
     "https://boot.dev/dev/lew",
     "https://boot.dev/dev/ref",
+    "https://boot.dev/posts/zen-of-proverbs/",
   ];
   expect(getURLsFromHTML(baseAnchorTag, "https://boot.dev")).toStrictEqual(
     links,
--- src/utils/getURLsFromHTML.ts
+++ src/utils/getURLsFromHTML.ts
@@ -4,10 +4,10 @@ export function getURLsFromHTML(html: string, rootUrl: string) {
   const dom = new JSDOM(html);
   const anchors = dom.window.document.querySelectorAll("a");
   const collector = [] as string[];
+
   anchors.forEach((anchor) => {
     try {
-      const myURL = new URL(rootUrl);
-      myURL.pathname = anchor.pathname;
+      const myURL = new URL(anchor.href, rootUrl);
       collector.push(myURL.href);
     } catch (e) {
       console.error(e);
